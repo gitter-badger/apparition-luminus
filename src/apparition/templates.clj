071f49f5670c366c457714d7213da642e1c14ad4
Odomontois <odomontois@gmail.com>
2014-10-16 18:13:49 +0400

    do not know what happened here

--- src/apparition/templates.clj
+++ src/apparition/templates.clj
@@ -5,19 +5,23 @@
             [hiccup.page :refer :all]))
 
 (defn base [template & [params]] (html5
-  [:head [:title "Welcome to apparition" ]]
+  [:head 
+    [:title "Welcome to apparition" ]]
   [:body
-  [:div.navbar.navbar-inverse.navbar-fixed-top [:div.container
-    [:div.navbar-header [:a.navbar-brand {:href "/"} "apparition"]]
-    [:div.navbar-collapse.collapse [:ul.nav.navbar-nav
-      (for [[el text sub] [["home" "Home" ""] ["about" "About"] ["some" "Some Dev Work"]]]
-        (let [sub (if (nil? sub) el sub)]
-          [:li {:class (str el "-selected")} [:a {:href (str "/" sub)} text]]))]]]]
-  [:div.container template]
-  (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
-    "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css")
-  (include-js "//code.jquery.com/jquery-2.1.1.min.js"
-    "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")]))
+    [:div.navbar.navbar-inverse.navbar-fixed-top 
+      [:div.container
+        [:div.navbar-header 
+          [:a.navbar-brand {:href "/"} "apparition"]]
+        [:div.navbar-collapse.collapse [:ul.nav.navbar-nav
+          (for [[el text sub] [["home" "Home" ""] ["about" "About"] ["some" "Some Dev Work"]]]
+            (let [sub (if (nil? sub) el sub)]
+              [:li {:class (str el "-selected")} 
+                [:a {:href (str "/" sub)} text]]))]]]]
+    [:div.container template]
+    (include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
+      "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css")
+    (include-js "//code.jquery.com/jquery-2.1.1.min.js"
+      "//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js")]))
 
-(defn some-page [& [params]] (base [:p [:h1 "Hello, Bro 1!!!"]] params))
+(defn some-page [& [params]] (base [:p "Hello, bro1"] params))
 
