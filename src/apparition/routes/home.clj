2468249ffc26127c1a4df282e13882b8ecab486b
Odomontois <odomontois@gmail.com>
2014-10-17 18:24:49 +0400

    few decorations

--- src/apparition/routes/home.clj
+++ src/apparition/routes/home.clj
@@ -1,8 +1,8 @@
 (ns apparition.routes.home
   (:require [compojure.core :refer :all]
-            [apparition.layout :as layout]
-            [apparition.templates :as pages]))
+            [apparition.layout :refer [page]]
+            [apparition.templates :as template]))
 
 (defroutes home-routes
-  (GET "/" [] (layout/page pages/home))
-  (GET "/about" [] (layout/page pages/about)))
+  (GET "/" [] (page template/home))
+  (GET "/about" [] (page template/about)))
