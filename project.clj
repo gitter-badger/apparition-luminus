0a19bc39424eaba54ad8a50c15541f278c674ee3
Odomontois <odomontois@gmail.com>
2014-11-11 17:17:52 +0300

    play added

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject apparition "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
                  [lib-noir "0.9.3"]
                  [ring-server "0.3.1"]
                  [selmer "0.7.2"]
